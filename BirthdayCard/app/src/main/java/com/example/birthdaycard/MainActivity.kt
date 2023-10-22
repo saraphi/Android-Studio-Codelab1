79ed53eddc8ccba2f89663a318514a23b483fd9c
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-10-22 17:22:37 +0100

    birthday card done

--- BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ BirthdayCard/app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.birthdaycard
 
+import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,25 +42,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HappyBirthday(name: String, from: String, modifier: Modifier = Modifier) {
+fun HappyBirthdayText(name: String, from: String, modifier: Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
         Text(
             text = "Happy birthday $name!",
-            modifier = modifier,
-            fontSize = 100.sp,
+            fontSize = 90.sp,
             lineHeight = 116.sp,
             textAlign = TextAlign.Center
         )
         Text(
             text = "from $from",
             fontSize = 36.sp,
-            modifier = modifier
+            modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End),
-            fontStyle = FontStyle.Italic
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun HappyBirthday(name: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        HappyBirthdayText(
+            name = name,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
